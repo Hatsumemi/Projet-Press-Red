cdc0b505c589a13703c7e204a29f5a978376d20b
P3D\judith.garnier <judithgarnier4@gmail.com>
2024-12-10 18:34:31 +0100

    photography trigger zone + photo limit

--- Assets/__Script/Photography.cs
+++ Assets/__Script/Photography.cs
@@ -2,22 +2,43 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using DG.Tweening;
+using UnityEngine.UI;
 
 public class Photography : MonoBehaviour
 {
 
-    public bool IsActive;
+    public bool IsActive = false;
+    public bool Triggered = false;
+    public Image Flash;
+    [SerializeField] private int _photoToTake;
+    private int _photoCount;
+
 
     void Start()
     {
-        
+        Flash.color = new Color(Flash.color.r, Flash.color.g, Flash.color.b, 0f);
     }
 
     void Update()
     {
-        if(IsActive && Input.GetMouseButtonDown(0))
+        if (IsActive && Input.GetMouseButtonDown(0))
         {
-
+            if (Triggered)
+            {
+                if (_photoCount < _photoToTake)
+                    StartCoroutine(WaitToPhotograph());
+                else
+                    Debug.Log("You can't take anymore photos.");
+            }
         }
     }
+
+    IEnumerator WaitToPhotograph()
+    {
+        Flash.DOFade(1, 0.2f);
+        yield return new WaitForSeconds(0.2f);
+        _photoCount++;
+        Flash.DOFade(0, 0.2f);
+    }
+
 }
---
+++ Assets/__Script/TriggerZone.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerZone : MonoBehaviour
+{
+    public Types Type;
+    public GameObject Player;
+    public enum Types
+    {
+        Photography,
+        Development,
+    }
+
+
+    void Start()
+    {
+
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other != null && other.gameObject == Player)
+        {
+            if (Type == Types.Photography)
+                MainGame.Instance.m_Photography.Triggered = true;
+            if (Type == Types.Development)
+                Debug.Log("Not coded yet.");
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        MainGame.Instance.m_Photography.Triggered = false;
+    }
+}
