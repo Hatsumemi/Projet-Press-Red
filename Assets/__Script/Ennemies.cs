ce582e862bf2395ec9b2257734a384cd43b8ccd7
Hatsumemi/Judith <judithgarnier4@gmail.com>
2025-01-03 16:47:48 +0100

    Functional Enemies movement

--- Assets/__Script/Ennemies.cs
+++ Assets/__Script/Ennemies.cs
@@ -10,51 +10,57 @@ public class Ennemies : MonoBehaviour
     public List<GizmoToMove> GizmosMovement;
     [SerializeField] private float _speed;
     private bool _hasDetectedPlayer = false;
+    private bool _onFirstRound = true;
     private GameObject _target;
-    private int _gizmosCount = 0;
-
-
-    // Update is called once per frame
+    private int _currentWaypointIndex = 0; 
+    
     void Update()
     {
         if (_hasDetectedPlayer)
         {
-            Debug.Log("The player has been detected.");
+            Debug.Log("Le joueur a été détecté.");
             _hasDetectedPlayer = false;
         }
 
         if (CanMove)
         {
-            if (_gizmosCount < GizmosMovement.Count)
-            {
-                for (int i = 0; i < GizmosMovement.Count; i++)
-                {
-                    _target = GizmosMovement[i].gameObject;
-                    if (transform.position == _target.transform.position)
-                    {
-                        _gizmosCount++;
-                    }
-
-                    float step = _speed * Time.deltaTime;
-                    transform.position = Vector3.MoveTowards(transform.position, _target.transform.position, step);
-                }
-            }
-
-            if (_gizmosCount > 0)
-            {
-                for (int i = GizmosMovement.Count - 1; i > 0; i--)
-                {
-                    _target = GizmosMovement[i].gameObject;
-                    if (transform.position == _target.transform.position)
-                    {
-                        _gizmosCount--;
-                    }
-
-                    float step = _speed * Time.deltaTime;
-                    transform.position = Vector3.MoveTowards(transform.position, _target.transform.position, step);
-                }
-            }
+            MoveTowardsWaypoint();
         }
+    }
+
+    private void MoveTowardsWaypoint()
+    {
+        if (_currentWaypointIndex < 0)
+            _currentWaypointIndex = 0;
+        else if (_currentWaypointIndex == GizmosMovement.Count)
+            _currentWaypointIndex = GizmosMovement.Count-1;
+        _target = GizmosMovement[_currentWaypointIndex].gameObject;
+
+            if (Vector3.Distance(transform.position, _target.transform.position) > 0.1f)
+            {
+                float step = _speed * Time.deltaTime;
+                transform.position = Vector3.MoveTowards(transform.position, _target.transform.position, step);
+            }
+            else
+            {
+                if (_onFirstRound)
+                {
+                    _currentWaypointIndex++; 
+                    if (_currentWaypointIndex == GizmosMovement.Count)
+                    {
+                        _onFirstRound = false; 
+                    }
+                }
+                else if (_onFirstRound == false)
+                {
+                    _currentWaypointIndex--; 
+                    if (_currentWaypointIndex == 0) 
+                    {
+                        _onFirstRound = true; 
+                    }
+                }
+            }
+        
     }
 
     private void OnTriggerEnter(Collider other)
