9232fbfd4436c57401c09c3cab6ac02068f79c6a
Velmeti <mr.velmeti@gmail.com>
2025-01-20 10:35:29 +0100

    First person movement

--- Assets/__Script/Character/GizmosPlayerCam.cs
+++ Assets/__Script/Character/GizmosPlayerCam.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
+using Unity.VisualScripting.Antlr3.Runtime.Tree;
 using UnityEngine;
 using UnityEngine.TextCore.Text;
 
@@ -13,6 +14,7 @@ public class GizmosPlayerCam : MonoBehaviour
 
     [SerializeField] private Transform _head;
     [SerializeField] private Transform _transformCamera;
+    [SerializeField] private float _distance;
 
     void Update()
     {
@@ -33,26 +35,40 @@ public class GizmosPlayerCam : MonoBehaviour
 
 
 
-        if (MainGame.Instance.m_PlayerController.IsCrouching == false)
-        {
+        //if (MainGame.Instance.m_PlayerController.IsCrouching == false)
+        //{
             
-            transform.position = new Vector3(_head.position.x + GizmosX,
+            /*transform.position = new Vector3(_head.position.x + GizmosX,
                 _head.position.y + GizmosY,
-            _head.position.z + GizmosZ);
+            _head.position.z + GizmosZ);*/
 
 
             /*if (_head == null || _transformCamera == null)
                 return;
 
             Vector3 rightDirection = _transformCamera.right;
-            Vector3 newPosition = _head.position + rightDirection * GizmosX;
+            Vector3 newPosition = _head.position + rightDirection * _distance;
             transform.position = newPosition;*/
+        //}
+        //else
+        //{
+            /*transform.position = new Vector3(_head.position.x + GizmosX,
+            _head.position.y + GizmosYCrouch,
+            _head.position.z + GizmosZ);*/
+        //}
+
+        if (MainGame.Instance.m_Photography.IsActive == false)
+        {
+            if (_head == null || _transformCamera == null)
+                return;
+
+            Vector3 rightDirection = _transformCamera.right;
+            Vector3 newPosition = _head.position + rightDirection * _distance;
+            transform.position = newPosition;
         }
         else
         {
-            transform.position = new Vector3(_head.position.x + GizmosX,
-            _head.position.y + GizmosYCrouch,
-            _head.position.z + GizmosZ);
+            transform.position = new Vector3(_head.position.x, _head.position.y, _head.position.z);
         }
     }
 
